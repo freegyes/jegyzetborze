0783a38c548e6aba2a833def5d7deca58e2c6b08
freegyes <freegyes@gmail.com>
2015-03-11 21:34:41 +0100

    add searchstringquery

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -4,6 +4,7 @@ Template.home.created = function () {
   Session.set('id', '');
   Session.set('cover', '/moly-logo.jpeg');
   Session.set('link', 'http://moly.hu');
+  Session.set('results', []);
 };
 
 Template.home.helpers({
@@ -25,21 +26,21 @@ Template.home.helpers({
 });
 
 Template.home.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    
-    var book = {
-      title: $(e.target).find('[name=title]').val(),
-      author: $(e.target).find('[name=author]').val(),
-    };
-        
-    Meteor.call('bookInsert', book, function(error, result) {
-       if (error)
-         return console.log(error.reason);
-         //return throwError(error.reason, "danger");
-        //throwError("This new excellent thought was successfully added to your mind-palace.", "success");  
-    });
-  },
+  // 'submit form': function(e) {
+  //   e.preventDefault();
+  //   
+  //   var book = {
+  //     title: $(e.target).find('[name=title]').val(),
+  //     author: $(e.target).find('[name=author]').val(),
+  //   };
+  //       
+  //   Meteor.call('bookInsert', book, function(error, result) {
+  //      if (error)
+  //        return console.log(error.reason);
+  //        //return throwError(error.reason, "danger");
+  //       //throwError("This new excellent thought was successfully added to your mind-palace.", "success");  
+  //   });
+  // },
   'click .callServer': function () {
     var isbn = $('#isbn').val(); 
 
@@ -53,7 +54,7 @@ Template.home.events({
         //return throwError("Dunno this isbn", "warning");
       } else {
 
-        data = $.parseJSON(result.content);
+        var data = $.parseJSON(result.content);
 
         $('#author').val(data.author);
         $('#title').val(data.title);
@@ -76,6 +77,62 @@ Template.home.events({
 
       }
 
+    });
+
+  },
+  'click .searchServer': function(e) {
+    e.preventDefault();
+    var query = $('#search').val();
+    var searchResults = $('#searchResults');
+
+    searchResults.empty();
+
+    Meteor.call('searchMoly', query, function (error, result) {
+      if (error) {
+        return console.log(error.reason);
+      } else {
+        var data = $.parseJSON(result);
+        Session.set('results', data.books);
+        for (var i = 0; i < data.books.length; i++) {
+
+          var toAppend = "<a id=\"" + [i] + "\" class=\"list-group-item\"><strong>" + data.books[i].title + "</strong> - " + data.books[i].author + "</a>";
+          searchResults.append(toAppend);
+          
+        };
+      }
+    });
+  },
+  'click .list-group-item': function() {
+    var id = event.target.id;
+    var selected = Session.get('results');
+    var queryId = selected[id].id;
+
+    Meteor.call('checkMolyId', queryId, function(error, result) {
+      if (error) return console.log(error.reason);
+      //if (error) return throwError("Are you sure this is a valid URL?", "warning");
+
+      var data = $.parseJSON(result);
+
+      Session.set('link', data.book.url);
+      
+      var authors = [];
+      for (var i = 0; i < data.book.authors.length; i++) {
+        authors.push(data.book.authors[i].name);
+      };
+
+      authors = authors.join(", ");
+      console.log(authors);
+
+      $('#author').val(authors);
+      $('#title').val(data.book.title);
+
+      Session.set('author', authors);
+      Session.set('title', data.book.title);
+      if (data.book.cover) {Session.set('cover', data.book.cover);};
+      Session.set('id', data.book.id);
+
+      $('#searchResults').empty();
+      $('#search').val('');
     });
 
   }
